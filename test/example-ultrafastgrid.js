f226792d5d19ffa27f27fbdfb47efdbbf0bebfed
Colby Fayock <colbyfayock@users.noreply.github.com>
2021-06-09 14:46:58 -0400

    Update example-ultrafastgrid.js

--- test/example-ultrafastgrid.js
+++ test/example-ultrafastgrid.js
@@ -43,7 +43,7 @@ describe('wdio', function () {
         const configuration = new Configuration();
 
         // create a new batch info instance and set it to the configuration
-        configuration.setBatch(new BatchInfo('Ultrafast Batch - WDIO 4 - Ultrafast'))
+        configuration.setBatch(new BatchInfo('Demo Batch - WDIO 4 - Ultrafast'))
 
         // Add browsers with different viewports
         configuration.addBrowser(800, 600, BrowserType.CHROME);
